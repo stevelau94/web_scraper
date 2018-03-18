2be3bcfe7f04d94c32a7f461dbd266a205aa3aac
stevelau94 <stevelau94@gmail.com>
2018-03-18 20:18:49 +0800

    使用Selenium模拟chrome/phantomJS抓取淘宝商品美食信息,存入MongoDB

---
+++ 03_taobaoMeishi/spider.py
@@ -0,0 +1,97 @@
+import re
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from pyquery import PyQuery as pq
+from config import *
+import pymongo
+
+client = pymongo.MongoClient(MONGO_URL)
+db = client[MONGO_DB]
+
+browser = webdriver.PhantomJS(service_args=SERVICE_ARGS)
+wait = WebDriverWait(browser, 10)
+
+browser.set_window_size(1400, 900)
+
+def search():
+    print('正在搜索')
+    try:
+        browser.get('https://www.taobao.com')
+        input = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#q"))
+        )
+        submit = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_TSearchForm > div.search-button > button')))
+        input.send_keys(KEYWORD)
+        submit.click()
+        # 打开页面之后，等待网页打开然后获取总页数
+        total = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.total')))
+        get_products()
+        return total.text  # 由于容易出现timeouterror 所以需要捕捉异常
+    except TimeoutException:
+        return search()
+
+
+def next_page(page_number):
+    print('正在翻页', page_number)
+    try:
+        # 通过网页下方输入框输入页码
+        input = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#mainsrp-pager > div > div > div > div.form > input"))
+        )
+        submit = wait.until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.form > span.btn.J_Submit')))
+        input.clear()
+        input.send_keys(page_number)
+        submit.click()
+        # 判断页码是否已经跳转
+        wait.until(EC.text_to_be_present_in_element(
+            (By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > ul > li.item.active > span'), str(page_number)))
+        get_products()
+    except TimeoutException:  # 如果出错就重新调用此函数重新请求新页码
+        next_page(page_number)
+
+
+def get_products():
+    # 等待宝贝信息加载成功
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-itemlist .items .item')))
+    html = browser.page_source
+    doc = pq(html)
+    items = doc('#mainsrp-itemlist .items .item').items()
+    for item in items:
+        product = {
+            'image': item.find('.pic .img').attr('src'),
+            'price': item.find('.price').text(),
+            'deal': item.find('.deal-cnt').text()[:-3],
+            'title': item.find('.title').text(),
+            'shop': item.find('.shop').text(),
+            'location': item.find('.location').text()
+        }
+        print(product)
+        save_to_mongo(product)
+
+
+def save_to_mongo(result):
+    try:
+        if db[MONGO_TABLE].insert(result):
+            print('存储到MONGODB成功', result)
+    except Exception:
+        print('存储失败', result)
+
+
+def main():
+    try:
+        total = search()
+        total = int(re.compile('(\d+)').search(total).group(1))
+        for i in range(2, total + 1):  # 翻页功能
+            next_page(i)
+    except Exception: print('出错了！')
+    finally:
+        browser.close()
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ some notebooks&amp;codes about web scraper
 1. 对BeautifulSoup,pyquery,requests,selenium等爬虫基本库的详细学习笔记。
 1. [实战]爬取猫眼top100电影数据，存入文件，引入进程池，多进程爬取
 1. [实战]分析Ajax来爬取今日头条街拍美图，存入MongoDB
+1. [实战]使用Selenium模拟chrome/phantomJS抓取淘宝商品美食信息,存入MongoDB
+    [Selenium with Python](http://selenium-python.readthedocs.io/)  
+    [phantomjs_API](http://phantomjs.org/api/command-line.html)
